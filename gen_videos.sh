51699d9fd48560b0ebbfebea3e6c27c76aacf443
Thomas Daede <daede003@umn.edu>
2018-01-05 15:19:01 -0800

    make it gooder

--- gen_videos.sh
+++ gen_videos.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-SOURCE="-y -f lavfi -i testsrc -frames 10"
+SOURCE="-y -f lavfi -i smptebars -frames 10"
+SRCCOLOR="iall=bt601-6-525"
+FAST=""
 
-ffmpeg $SOURCE -vf colorspace=iall=bt709:irange=pc:all=bt709:format=yuv420p:range=pc:fast=1 -frames 1 srgb.png
-ffmpeg $SOURCE -vf colorspace=iall=bt709:irange=pc:all=bt601-6-525:format=yuv420p:range=mpeg:fast=1 bt601_420.ogv
-ffmpeg $SOURCE -vf colorspace=iall=bt709:irange=pc:all=bt601-6-525:format=yuv420p:range=mpeg:fast=1 -c:v libvpx bt601_420_vp8.webm
-ffmpeg $SOURCE -vf colorspace=iall=bt709:irange=pc:all=bt601-6-525:format=yuv420p:range=mpeg:fast=1 -c:v libvpx-vp9 bt601_420_vp9.webm
-ffmpeg $SOURCE -vf colorspace=iall=bt709:irange=pc:all=bt709:format=yuv420p:range=mpeg:fast=1 -c:v libvpx-vp9 bt709_420_vp9.webm
-ffmpeg $SOURCE -vf colorspace=iall=bt709:irange=pc:all=bt2020:format=yuv420p:range=mpeg:fast=1 -c:v libvpx-vp9 bt709_420_vp9.webm
+ffmpeg $SOURCE -vf colorspace=$SRCCOLOR:all=bt709:format=yuv420p:range=pc:$FAST -frames 1 srgb.png
+ffmpeg $SOURCE -vf colorspace=$SRCCOLOR:all=bt601-6-525:format=yuv420p:range=mpeg:$FAST bt601_420.ogv
+ffmpeg $SOURCE -vf colorspace=$SRCCOLOR:all=bt601-6-525:format=yuv420p:range=mpeg:$FAST -c:v libvpx bt601_420_vp8.webm
+ffmpeg $SOURCE -vf colorspace=$SRCCOLOR:all=bt601-6-525:format=yuv420p:range=mpeg:$FAST -c:v libvpx-vp9 bt601_420_vp9.webm
+ffmpeg $SOURCE -vf colorspace=$SRCCOLOR:all=bt709:format=yuv420p:range=mpeg:$FAST -c:v libvpx-vp9 bt709_420_vp9.webm
+ffmpeg $SOURCE -vf colorspace=$SRCCOLOR:all=bt2020:format=yuv420p:range=mpeg:$FAST -c:v libvpx-vp9 bt2020_420_vp9.webm
